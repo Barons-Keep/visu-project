b631458a35a7c010238efd959e62a62da6933742
Alkapivo <alkapivo@gmail.com>
2025-11-11 22:41:45 +0100

    Copy SFX from visu-gml to yyp/datafiles

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ gm-cli install
 gm-cli sync
 rm -rf ./yyp/datafiles/track
 cp -rv ./gm_modules/track/resource/datafiles/track/ ./yyp/datafiles/track/
-
+cp -rf ./gm_modules/visu/resource/sound/sfx ./yyp/datafiles
